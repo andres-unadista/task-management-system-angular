0ca25f95ac05e206c63f57809c234f5bbdf7fc2f
Fernández <affernandez999@gmail.com>
2025-05-26 20:43:03 -0500

    feat: Add autoprefixer and enable darkMode

--- taildwind.config.js
+++ taildwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: 'media', // or 'class'
   theme: {
     extend: {},
   },
   plugins: [],
+  // This is an optional comment
 }
